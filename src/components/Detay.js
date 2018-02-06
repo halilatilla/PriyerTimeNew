b0aafb23cb7c7e635282399ff9adbe32e4e51177
Apple MacBook Pro <atillahalil0@gmail.com>
2018-02-06 18:00:07 +0300

    button added

--- src/components/Detay.js
+++ src/components/Detay.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Platform } from 'react-native';
+import { Text, View, StyleSheet, Platform, Button } from 'react-native';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import { Actions } from 'react-native-router-flux';
 import { dataChange } from '../redux/actions/index';
 
 
@@ -15,27 +16,36 @@ class Detay extends Component {
                   });
       }
 
+      buttonMain = () => {
+            Actions.Ulke();
+      }
+
       render() {
             const mapGelenData = this.props.datavakitler.map((resp, id) => {
                   if (id === 0) {
-                        return (<View key={id} style={styles.subcontainerStyle}> 
-                        <Text style={styles.textStyle}>İmsak    -     {resp.Imsak}</Text>
-                        <Text style={styles.textStyle}>Güneş    -    {resp.Gunes}</Text>
-                        <Text style={styles.textStyle}>Öğle       -    {resp.Ogle}</Text>
-                        <Text style={styles.textStyle}>İkindi     -     {resp.Ikindi}</Text>
-                        <Text style={styles.textStyle}>Akşam   -    {resp.Aksam}</Text>
-                        <Text style={styles.textStyle}>Yatsı      -     {resp.Yatsi}</Text>
-                              </View>);
+                        return (<View key={id} style={styles.subcontainerStyle}>
+                              <Text style={styles.textStyle}>İmsak    -     {resp.Imsak}</Text>
+                              <Text style={styles.textStyle}>Güneş    -    {resp.Gunes}</Text>
+                              <Text style={styles.textStyle}>Öğle       -    {resp.Ogle}</Text>
+                              <Text style={styles.textStyle}>İkindi     -     {resp.Ikindi}</Text>
+                              <Text style={styles.textStyle}>Akşam   -    {resp.Aksam}</Text>
+                              <Text style={styles.textStyle}>Yatsı      -     {resp.Yatsi}</Text>
+                        </View>);
                   }
             });
             return (
                   <View>
-
-                        <View style={{ alignItems: 'center', margin: 10, padding: 10 }} >
-                              <Text> {this.props.ulkeisim} </Text>
-                              <Text> {this.props.sehirisim} </Text>
-                              <Text> {this.props.ilcead} </Text>
+                        <View style={styles.viewStyle} >
+                              <Text style={styles.textSecond}> {this.props.ulkeisim} </Text>
+                              <Text style={styles.textSecond}> {this.props.sehirisim} </Text>
+                              <Text style={styles.textSecond}> {this.props.ilcead} </Text>
                         </View>
+                        <View>
+                              <Button onPress={this.buttonMain} title='Değiştir' />
+
+                        </View>
+
+
                         <View style={styles.containerStyle}>
                               {mapGelenData}
                         </View>
@@ -61,25 +71,33 @@ const styles = StyleSheet.create({
             marginTop: Platform.OS === 'ios' ? 50 : 10,
             alignItems: 'center'
 
-        
-          },
-          subContainerStyle: {
-          borderBottomWidth: 1,
-          padding: 5,
-          backgroundColor: '#fff',
-          justifyContent: 'flex-start',
-          flexDirection: 'row',
-          borderColor: '#ddd',
-          position: 'relative',
-          alignItems: 'center'
-        },
+
+      },
+      subContainerStyle: {
+            borderBottomWidth: 1,
+            padding: 5,
+            backgroundColor: '#fff',
+            justifyContent: 'flex-start',
+            flexDirection: 'row',
+            borderColor: '#ddd',
+            position: 'relative',
+            alignItems: 'center'
+      },
       textStyle: {
             fontSize: 30,
             padding: 10,
             fontWeight: 'bold',
             alignItems: 'center'
+      },
+      textSecond: {
+            fontSize: 20,
+            padding: 5,
+            fontWeight: 'bold',
+            alignItems: 'center'
+      },
+      viewStyle: {
+            alignItems: 'center'
 
-            
       }
 }
 );
