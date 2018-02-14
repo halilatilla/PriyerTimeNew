e2c2a586e73d6f764f846a5e996990b16407607e
Apple MacBook Pro <atillahalil0@gmail.com>
2018-02-14 14:01:45 +0300

    portraitmodeOK

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { View, ActivityIndicator, Text } from 'react-native';
 
 const Spinner = () => {
     return (
         <View>
             <ActivityIndicator size={'large'} />
+            <Text> İnternete Bağlanılıyor</Text>
+            <Text>    Lütfen Bekleyiniz...</Text>
         </View>
 
     );
