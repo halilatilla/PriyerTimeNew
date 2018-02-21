117d039337446fef23edc6ae9a769d5793c9654e
Apple MacBook Pro <atillahalil0@gmail.com>
2018-02-21 17:35:23 +0300

    Splash Screen Added

---
+++ android/app/src/main/java/com/priyertimenew/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.priyertimenew;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by applemacbookpro on 21.02.2018.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
